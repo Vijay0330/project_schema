95feeecd610d6748cd01894c4eccdb2432c3976c
Vijay A <vijay@techversantinfo.com>
2021-01-27 10:30:20 +0530

    method added

--- product.model.js
+++ product.model.js
@@ -57,71 +57,21 @@ const productSchema = new Schema({
 });
 
 
+//filtered by category
+productSchema.methods.findSimilarCategory =  (category,cb)=> {
+  return this.model('product').find({ category: category }, cb);
+};
 
+//offer product
+productSchema.methods.findSimilarDiscount =  (cb)=> {
+  return this.model('product').find({ discount: {$gt:40} }, cb);
+};
 
+//filter by price
+productSchema.methods.filterByPrice =  (price,cb)=> {
+  return this.model('product').find({ discount: {$lt:price} }, cb);
+};
 
-productSchema.statics = {
-
-    /**
-     * Get Course
-     *
-     * @param {ObjectId} id - The objectId of course.
-     * @returns {Promise<User, APIError>}
-     */
-    async get(id,imageStatus="enabled") {
-      try {
-        let product;
-        if (mongoose.Types.ObjectId.isValid(id)) {
-          if(imageStatus == "all"){
-            product = await this.findById(id).populate('category').populate('subCategory').populate('productVideos').populate('productPictures').exec();
-          } else {
-            product = await this.findById(id).populate('category').populate('subCategory')
-                                .populate({path : 'productPictures',
-                                   match : {status : imageStatus} 
-                                  })
-                                .populate({path : 'productVideos',
-                                   match : {status : imageStatus} 
-                                  })
-                                .exec();
-          }
-        }
-        if (product) {
-          return product;
-        }
-  
-        throw new APIError({
-          message: 'Product does not exist',
-          status: httpStatus.NOT_FOUND,
-        });
-      } catch (error) {
-        throw error;
-      }
-    },
-  
-    /**
-     * Return new validation error
-     * if error is a mongoose duplicate key error
-     *
-     * @param {Error} error
-     * @returns {Error|APIError}
-     */
-    checkDuplicateName(error) {
-      if (error.name === 'MongoError' && error.code === 11000) {
-        return new APIError({
-          message: 'Validation Error',
-          errors: [{
-            field: 'name',
-            location: 'body',
-            messages: ['name already exists'],
-          }],
-          status: httpStatus.CONFLICT,
-          isPublic: true,
-          stack: error.stack,
-        });
-      }
-      return error;
-    }
-}
 const productModel = mongoose.model('product',productSchema);
 
 
